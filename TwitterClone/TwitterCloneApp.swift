9b23105a6d16393a31b3c9a4b235dae332ec6dc2
Koshenka27 <koshenka27@gmail.com>
2023-08-01 08:14:35 -0400

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Logan Koshenka on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
